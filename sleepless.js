cb7890d39dd9b781d64f0c93cc4bca564df746a4
Joe Hitchens <joe@sleepless.com>
2013-11-15 18:38:33 -0800

    updates

--- sleepless.js
+++ sleepless.js
@@ -21,89 +21,205 @@ IN THE SOFTWARE.
 */
 
 if((typeof process) === 'undefined') {
-	// browser
 	isBrowser = true;
 	isNode = false;
-	global = window
-
-	ID = {};
-	var a = document.getElementsByTagName("*");
-	var e, id;
-	for(var i = 0; i < a.length; i++)
-	{
-		e = a[i];
-		id = e.id;
-		if(id)
-			ID[id] = e;
-	}
-
+	global = window;	// so i can define global functions outside this module
 }
 else  {
-	// node
 	isNode = true;
 	isBrowser = false;
 }
-// -------
-global.throwIf = function(c, s) { if(c) { throw new Error(s || "FAILED ASSERTION"); } }
-global.nop = function(){}
-global.time = function() { return (new Date).uts() }
-global.j2o = function(j) { try { return JSON.parse(j) } catch(e) { return null } }
-global.o2j = function(o) { try { return JSON.stringify(o) } catch(e) { return null } }
-global.toInt = global.p10 = function(v) { return parseInt(""+v, 10) || 0 }
-global.toFloat = global.pflt = function(v) { return parseFloat(""+v) || 0.0 }
+
+
+
+
+// for convenience
 global.log = function(m) { return console.log(m); }
-global.money = function(n, dec, sep) {
-    if(sep === undefined)
-        sep = ",";
-    if(dec === undefined)
-        dec = ".";
-    var n = Math.round( pflt(n) * 100 ) / 100;
-    var sign = n < 0 ? '-' : '';
-    n = Math.abs(+n || 0);
-    var intPart = parseInt(n.toFixed(2), 10) + '';
-    var j = intPart.length > 3 ? intPart.length % 3 : 0;
-    return sign + (j ? intPart.substr(0, j) + sep : '') + intPart.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + sep) + (2 ? dec + Math.abs(n - intPart).toFixed(2).slice(2) : '');
-}
-global.c2b = function(cents) {
-    return money(cents / 100, ".", "");
-}
-global.b2c = function(bucks) {
-    return parseFloat( ((""+bucks).replace(/[^-.0-9]+/g, "")) ) * 100;
-}
+
+// throw an error if a condition is true
+global.throwIf = function(c, s) { if(c) { throw new Error(s || "FAILED ASSERTION"); } }
+
+// a default no-op function for callbacks
+global.nop = function(){}
+
+// return json as object or null if error
+global.j2o = function(j) { try { return JSON.parse(j) } catch(e) { return null } }
+
+// return object as JSON or null if error  
+global.o2j = function(o) { try { return JSON.stringify(o) } catch(e) { return null } }
 
 
-// -------
-/*
-Object.prototype.json = function() { return o2j(this); }
-Object.prototype.toInt = function() { return p10(this.toString()) }
-Object.prototype.toFloat = function() { return pflt(this.toString()) }
-Object.prototype.toMoney = function(decPoint, thousandsSep) {
-	var n = Math.round( this.toFloat() * 100 ) / 100;
-    var sign = n < 0 ? '-' : '';
-    n = Math.abs(+n || 0);
-    var intPart = parseInt(n.toFixed(2), 10) + '';
-    var j = intPart.length > 3 ? intPart.length % 3 : 0;
-    return sign + (j ? intPart.substr(0, j) + "," : '') + intPart.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + ",") + (2 ? "." + Math.abs(n - intPart).toFixed(2).slice(2) : '');
+
+
+// convert whatever to float
+global.toFlt = function(v) { return parseFloat((""+v).replace(/[^-.0-9]/g, "")) || 0.0; }
+
+// convert whatever to integer
+global.toInt = function(v) {
+	var n = toFlt(v);
+	return Math[n < 0 ? 'ceil' : 'floor'](n);
 };
-*/
-// -------
-Date.prototype.millis = function() { return (new Date()).getTime() }
-Date.prototype.uts = function() { return Math.floor(this.millis() / 1000) }	// unix timestamp; secs from epoch
-// -------
-String.prototype.trim = String.prototype.trim || function() {return this.replace(/^\s+|\s+$/g, "")}
-String.prototype.obj = function() { return j2o(this); }
-String.prototype.lower = function() { return this.toLowerCase() }
-String.prototype.upper = function() { return this.toUpperCase() }
-String.prototype.abbr = function(l) {
-	l = p10(l) || 5;
-	if(this.length <= l) {
-		return this;
-	}
-	return this.substr(0, l - 4) + " ...";
+
+// convert from cents to bucks
+global.centsToBucks = function(cents) {
+	return toFlt( toInt(cents) / 100 );
+    //return money(cents / 100, ".", "");
 }
-String.prototype.cap = String.prototype.ucfirst = function() {
+global.c2b = global.centsToBucks;
+
+// convert bucks to cents
+global.bucksToCents = function(bucks) {
+	return toInt( toFlt(bucks) * 100 );
+    //return parseFloat( ((""+bucks).replace(/[^-.0-9]+/g, "")) ) * 100;
+}
+global.b2c = global.bucksToCents;
+
+// convert whatever to a string that looks like "1,234.56"
+global.toMoney = function(n, decimal, separator) {
+    if(separator === undefined)
+        separator = ",";
+    if(decimal === undefined)
+        decimal = ".";
+    var n = Math.round( toFlt(n) * 100 ) / 100;
+    var sign = n < 0 ? '-' : '';
+    n = Math.abs(+n || 0);
+    var intPart = parseInt(n.toFixed(2), 10) + '';
+    var j = intPart.length > 3 ? intPart.length % 3 : 0;
+    return sign +
+		(j ? intPart.substr(0, j) + separator : '') +
+		intPart.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + separator) +
+		(2 ? decimal + Math.abs(n - intPart).toFixed(2).slice(2) : '');
+}
+
+
+
+
+// return "now" as unix timestamp
+global.time = function() { return toInt(new Date().getTime() / 1000); }
+
+// convert unix timestamp to Date 
+global.ts2dt = function(ts) {
+	ts = toInt(ts);
+	return ts ? new Date(ts * 1000) : null;
+}
+
+// convert Date to unix timestamp
+global.dt2ts = function(dt) {
+	dt = dt || new Date();
+	return toInt(dt.getTime() / 1000);
+}
+
+// convert "MM/DD/YYYY HH:MM:SS" to Date object or null if "s" can't be parsed
+// if year is 2 digits, it will try guess what you meant
+// time part (HH:MM:SS) can be omitted and seconds is optional
+global.us2dt = function(us) {
+
+	if(!us) {
+		return null;
+	}
+
+	var m = (""+us).split( /[^\d]+/ );
+
+	if(m.length < 3) {
+		return null;
+	}
+	while(m.length < 7) {
+		m.push("0");
+	}
+
+	// try to convert 2 digit year to 4 digits (best guess)
+	var year = toInt(m[2]);
+	var nowyear = new Date().getFullYear();
+	if(year <= ((nowyear + 10) - 2000))
+		year = 2000 + year;
+	if(year < 100)
+		year = 1900 + year;
+
+	var mon = toInt(m[0]) - 1;
+	var date = toInt(m[1]);
+
+	var hour = toInt(m[3]);
+	var min = toInt(m[4]);
+	var sec = toInt(m[5]);
+	var ms = toInt(m[6]);
+
+	return new Date(year, mon, date, hour, min, sec, ms);
+}
+
+
+// convert "MM/DD/YYYY HH:MM:SS" to unix timestamp
+global.us2ts = function(us) {
+	return dt2ts(us2dt(us));
+}
+
+// convert unix timestamp to "MM/DD/YYYY HH:MM:SS"
+global.ts2us = function(ts) {
+	var d = ts2dt(ts);
+	if(!d) {
+		return "";
+	}
+	return ""+
+		("0"+(d.getMonth() + 1)).substr(-2)+
+		"/"+
+		("0"+d.getDate()).substr(-2)+
+		"/"+
+		d.getFullYear()+
+		" "+ 
+		("0"+d.getHours()).substr(-2)+
+		":"+
+		("0"+d.getMinutes()).substr(-2)+
+		":"+
+		("0"+d.getSeconds()).substr(-2)+
+		"";
+}
+
+// convert unix timestamp to "MM/DD"
+global.ts2us_md = function(ts) {
+	return ts2us(ts).substr(0, 5);
+}
+
+// convert unix timestamp to "MM/DD/YYYY"
+global.ts2us_mdy = function(ts) {
+	return ts2us(ts).substr(0, 10);
+}
+
+// convert unix timestamp to "MM/DD/YY"
+global.ts2us_mdy2 = function(ts) {
+	var a = ts2us_mdy(ts).split("/");
+	a[2] = a[2].substr(2);
+	return a.join("/");
+}
+
+// convert unix timestamp to "HH:MM"
+global.ts2us_hm = function(ts) {
+	return ts2us(ts).substr(11, 5);
+}
+
+// convert unix timestamp to "MM/DD/YYYY HH:MM"
+global.ts2us_mdyhm = function(ts) {
+	return ts2us_mdy(ts) + " " + ts2us_hm(ts);
+}
+
+// convert unix timestamp to "MM/DD/YY HH:MM"
+global.ts2us_mdy2hm = function(ts) {
+	return ts2us_mdy2(ts) + " " + ts2us_hm(ts);
+}
+
+
+
+
+// make all lowercase
+String.prototype.lcase = function() { return this.toLowerCase() }
+
+// make all uppercase
+String.prototype.ucase = function() { return this.toUpperCase() }
+
+// capitalize first word
+String.prototype.ucfirst = function() {
 	return this.substring(0,1).toUpperCase() + this.substring(1)
 }
+
+// capitalize all words
 String.prototype.ucwords = function( sep ) {
 	sep = sep || /[\s]+/;
 	var a = this.split( sep );
@@ -112,9 +228,36 @@ String.prototype.ucwords = function( sep ) {
 	}
 	return a.join( " " );
 }
-// -------
+
+// abbreviate to l chars with ellipses
+String.prototype.abbr = function(l) {
+	l = toInt(l) || 5;
+	if(this.length <= l) {
+		return this;
+	}
+	return this.substr(0, l - 4) + " ...";
+}
+
+// convert a string from something like "prof_fees" to "Prof Fees"
+String.prototype.toLabel = function() {
+	var s = this.replace( /[_]+/g, " " );
+	s = s.ucwords();
+	return s;
+}
+
+// convert a string from something like "Prof. Fees" to  "prof_fees"
+String.prototype.toId = function() {
+	var s = this.toLowerCase();
+	s = s.replace( /[^a-z0-9]+/g, " " );
+	s = s.trim();
+	s = s.replace( /\s+/g, "_" );
+	return s;
+}
+
+
+
+
 if(isNode) {
-	// node
 
 	// XXX make XHR version of this for browser?
 	global.getFile = function(path, enc, cb) {
@@ -124,7 +267,44 @@ if(isNode) {
 		}
 		fs.readFile(path, enc, cb);
 	}
+
 }
 else  {
-	// browser
+	// isBrowser
+
+	/*ID = {};
+	var a = document.getElementsByTagName("*");
+	var e, id;
+	for(var i = 0; i < a.length; i++)
+	{
+		e = a[i];
+		id = e.id;
+		if(id)
+			ID[id] = e;
+	}*/
+
+	// return element with id
+	I = function(id) { return document.getElementById(id); }
+
+	// return value of element with id (generally an input type element)
+	V = function(id) { return I(id).value.trim(); }
+
+	// return value of element with id as unix timestamp
+	V.ts = function(id) { return us2ts(V(id)); }
+
+	// return value of element with id as integer
+	V.int = function(id) { return toInt(V(id)); }
+
+	// return value of element with id as float
+	V.flt = function(id) { return toFlt(V(id)); }
+
+	// navigate to new url
+	jmp = function(url) { document.location = url; }
+
+	// reload page
+	reload = function() { document.location.reload(); }
+
+	// xxx 
+	getFile = function(path, enc, cb) { }
 }
+
--- test.js
+++ test.js
@@ -1,51 +1,84 @@
 
 var l = console.log
-s = require("./sleepless.js")
+require("./sleepless.js")
 
 o = { a:[1,2,3], t:true, f:false, o:{key:'val'}, pi:3.1415, n:null };
 j = JSON.stringify(o);
 
+throwIf( typeof nop !== "function" );
+
 throwIf( o2j(o) !== j );
 throwIf( o2j(j2o(j)) !== j );
 
-throwIf( "foO".lower() !== "foo" );
-throwIf( "foO".upper() !== "FOO" );
-throwIf( "foO".cap() !== "FoO" );
+throwIf( toInt(3) !== 3 );
+throwIf( toInt(3.0) !== 3 );
+throwIf( toInt("3") !== 3 );
+throwIf( toInt("1,234") !== 1234 );
+throwIf( toInt("1,234.56") !== 1234 );
+throwIf( toInt("-1,234.56") !== -1234 );
+throwIf( toInt(-1234.56) !== -1234 );
+
+throwIf( toFlt(3) !== 3 );
+throwIf( toFlt(3.0) !== 3 );
+throwIf( toFlt("3") !== 3 );
+throwIf( toFlt("1,234") !== 1234 );
+throwIf( toFlt("1,234.56") !== 1234.56 );
+throwIf( toFlt("-1,234.56") !== -1234.56 );
+throwIf( toFlt(-1234.56) !== -1234.56 );
+
+throwIf( c2b("10") !== 0.1 );
+throwIf( c2b("12345") !== 123.45 );
+throwIf( c2b(12345) !== 123.45 );
+throwIf( c2b(1) !== 0.01 );
+throwIf( b2c("100") !== 10000 );
+throwIf( b2c("1.23") !== 123 );
+throwIf( b2c("1,234.56") !== 123456 );
+throwIf( b2c("1,234.5") !== 123450 );
+throwIf( b2c("1,234") !== 123400 );
+throwIf( b2c("0.01") !== 1 );
+throwIf( b2c("0.01") !== 1 );
+
+throwIf( toMoney("0.01") !== "0.01", "0.01" );
+throwIf( toMoney("31415.015") !== "31,415.02" );
+throwIf( toMoney("31415.01") !== "31,415.01" );
+throwIf( toMoney("31415.99") !== "31,415.99" );
+throwIf( toMoney(0.01) !== "0.01");
+throwIf( toMoney(31415.015) !== "31,415.02");
+throwIf( toMoney(31415.01) !== "31,415.01" );
+throwIf( toMoney(31415.99) !== "31,415.99" );
+throwIf( toMoney(-0.01) !== "-0.01" );
+throwIf( toMoney(-31415.016) !== "-31,415.02" );
+throwIf( toMoney(-31415.01) !== "-31,415.01" );
+throwIf( toMoney(-31415.99) !== "-31,415.99" );
+
+throwIf( time() !== dt2ts() );
+throwIf( ts2us(1384565221) !== "11/15/2013 17:27:01" );
+throwIf( ts2us_md(1384565221) !== "11/15" );
+throwIf( ts2us_mdy(1384565221) !== "11/15/2013" );
+throwIf( ts2us_mdy2(1384565221) !== "11/15/13" );
+throwIf( ts2us_hm(1384565221) !== "17:27" );
+throwIf( ts2us_mdyhm(1384565221) !== "11/15/2013 17:27" );
+throwIf( ts2us_mdy2hm(1384565221) !== "11/15/13 17:27" );
+
+throwIf( !( us2dt("11/15/2013 17:27:01") instanceof Date) );
+throwIf( dt2ts( us2dt("11/15/2013 17:27:01") ) !== 1384565221 );
+
+throwIf( "foO".lcase() !== "foo" );
+throwIf( "foO".ucase() !== "FOO" );
 throwIf( "foO".ucfirst() !== "FoO" );
 throwIf( "foo baR".ucwords() !== "Foo BaR" );
 throwIf( "foo bar baz".abbr(7) !== "foo ..." );
 throwIf( "foo".abbr(7) != "foo" );		// XXX why doesn't this work with !== ?
 throwIf( " \tfoo bar \n".trim() !== "foo bar" );
-throwIf( toInt("3.1415") !== 3 );
-throwIf( toFloat("3.1415") !== 3.1415 );
 
-throwIf( money("0.01") !== "0.01", "0.01" );
-throwIf( money("31415.015") !== "31,415.02" );
-throwIf( money("31415.01") !== "31,415.01" );
-throwIf( money("31415.99") !== "31,415.99" );
-throwIf( money(0.01) !== "0.01");
-throwIf( money(31415.015) !== "31,415.02");
-throwIf( money(31415.01) !== "31,415.01" );
-throwIf( money(31415.99) !== "31,415.99" );
-throwIf( money(-0.01) !== "-0.01" );
-throwIf( money(-31415.016) !== "-31,415.02" );
-throwIf( money(-31415.01) !== "-31,415.01" );
-throwIf( money(-31415.99) !== "-31,415.99" );
+throwIf( "Foo Bar".toId() !== "foo_bar" );
+throwIf( "Foo_Bar".toId() !== "foo_bar" );
+throwIf( " Foo_Bar ! ".toId() !== "foo_bar" );
+throwIf( "foo_bar".toLabel() !== "Foo Bar" );
+throwIf( "foo_bar".toLabel() !== "Foo Bar" );
+throwIf( "foo_bar!".toLabel() !== "Foo Bar!" );
 
-throwIf( c2b("10") != 0.1 );
-throwIf( c2b("12345") != 123.45 );
-throwIf( c2b(12345) != 123.45 );
-throwIf( c2b(1) != 0.01 );
-throwIf( b2c("100") != 10000 );
-throwIf( b2c("1.23") != 123 );
-throwIf( b2c("1,234.56") != 123456 );
-throwIf( b2c("1,234.5") != 123450 );
-throwIf( b2c("1,234") != 123400 );
-throwIf( b2c("0.01") != 1 );
-throwIf( b2c("0.01") != 1 );
-
-
-getFile("test.js", function(err, data) {
+getFile("test.js", "utf8", function(err, data) {
 	throwIf( err, err );
 	throwIf( getFile("test.js").toString() !== data.toString() );
 });
