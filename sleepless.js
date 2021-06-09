6ed566cc266544fe24a05275ad1b3f1e91b802a2
Joe Hitchens <joe@sleepless.com>
2021-06-09 16:52:40 -0700

    -

--- sleepless.js
+++ sleepless.js
@@ -1,5 +1,8 @@
-/*
-Copyright 2020 Sleepless Software Inc. All rights reserved.
+/**
+
+@preserve
+
+Copyright 2021 Sleepless Software Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
@@ -18,6 +21,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE. 
+
 */
 
 (function() {
